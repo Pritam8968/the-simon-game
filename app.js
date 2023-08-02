81bfb6a8279028fe6a421bd53ead1987c8cabe6f
Pritam Sarkar <pritamsarkar9883@gmail.com>
2023-08-02 21:49:34 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+const gameSeq = [];
+const userSeq = [];
+const colors = ["green", "red", "yellow", "blue"];
+const levelBoard = document.querySelector(".level");
+const title = document.querySelector(".title");
+const buttons = document.querySelectorAll(".btn");
+
+let level = 0;
+let started = false;
+let highScore = 0;
+let score = 0;
+
+document.addEventListener("keypress", startGame);
+document.querySelector(".mobile-btn").addEventListener("click", startGame);
+
+function startGame() {
+	if (!started) {
+		started = true;
+		title.style.visibility = "hidden";
+		levelUp();
+		for (const btn of buttons) {
+			btn.addEventListener("click", buttonPress);
+		}
+	}
+}
+function levelUp() {
+	userSeq.splice(0, userSeq.length);
+	level++;
+	levelBoard.innerText = `Level ${level}`;
+
+	// selecting random button and flash it
+	let randomColor = colors[Math.floor(Math.random() * 4)];
+	makeSound(randomColor);
+	gameSeq.push(randomColor);
+	let btn = document.querySelector(`.${randomColor}`);
+	flashButton(btn);
+	console.log(gameSeq);
+}
+
+function flashButton(btn) {
+	btn.classList.add("flash");
+	setTimeout(() => {
+		btn.classList.remove("flash");
+	}, 250);
+}
+
+function buttonPress() {
+	let btn = this;
+	let btnId = btn.getAttribute("id");
+	makeSound(btnId);
+	userSeq.push(btnId);
+	// console.log(userSeq);
+	flashButton(btn);
+	checkAns(userSeq.length - 1);
+}
+
+function checkAns(lvl) {
+	if (userSeq[lvl] === gameSeq[lvl]) {
+		if (userSeq.length === gameSeq.length) {
+			console.log("correct");
+			score += level;
+			setTimeout(() => {
+				levelUp();
+			}, 1000);
+		} else {
+			console.log("continue");
+		}
+	} else {
+		gameOver();
+	}
+}
+
+function makeSound(name) {
+	let sound = new Audio(`./sounds/${name}.mp3`);
+	sound.play();
+}
+
+function gameOver() {
+	makeSound("wrong");
+	flashScreen();
+	let deviceWidth = window.innerWidth;
+	console.log(deviceWidth);
+	let restartMsg = "any key";
+	if (deviceWidth < 550) {
+		restartMsg = "start button";
+	}
+	highScore = Math.max(highScore, score);
+	levelBoard.innerText = `Score ${score}`;
+	title.innerHTML = `<b>Game Over!</b> High score: <b> ${highScore}</b> <br/>
+	Press ${restartMsg} to restart the game.`;
+	// reset everything
+	score = 0;
+	level = 0;
+	gameSeq.splice(0, gameSeq.length);
+	started = false;
+	title.style.visibility = "visible";
+}
+
+function flashScreen() {
+	let body = document.querySelector("body");
+	body.classList.add("flash-screen");
+	setTimeout(() => {
+		body.classList.remove("flash-screen");
+	}, 150);
+}
